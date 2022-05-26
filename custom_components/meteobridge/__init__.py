490a55ba348fbb7d60f7ef757bb946ea415c4fcf
Bjarne Riis <bjarne@briis.com>
2022-05-26 04:13:14 +0000

    Fixing deprecated async_get_registry that will start showing up in HA 2022.6

--- custom_components/meteobridge/__init__.py
+++ custom_components/meteobridge/__init__.py
@@ -131,7 +131,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def _async_get_or_create_nvr_device_in_registry(
     hass: HomeAssistant, entry: ConfigEntry, device_data: DataLoggerDescription
 ) -> None:
-    device_registry = await dr.async_get_registry(hass)
+    device_registry = dr.async_get(hass)
 
     device_registry.async_get_or_create(
         config_entry_id=entry.entry_id,
